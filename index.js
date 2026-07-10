4d734e00bfcc29f49d0aec3b1844f20869a6264a
GALEAM-create <recepcion.patriotismo@galeam.mx>
2026-07-10 17:04:31 -0600

    Remove dotenv error handling
    Removed dotenv configuration handling for missing module.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-try {
-  require("dotenv").config();
-} catch (error) {
-  if (error.code !== "MODULE_NOT_FOUND") {
-    throw error;
-  }
-
-  console.log(
-    "dotenv no está instalado; se usarán las variables proporcionadas por Railway."
-  );
-}
 
 const crypto = require("crypto");
 const fs = require("fs");
